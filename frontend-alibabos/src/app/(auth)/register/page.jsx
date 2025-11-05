05a119b95c34731985dc937c93155e313c39644c
zolangivre <130605937+zolangivre@users.noreply.github.com>
2025-11-06 00:10:32 +0100

    Revert "Feature/profile page"

--- frontend-alibabos/src/app/(auth)/register/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-function RegisterPage() {
-  return <div>Register Page</div>;
-}
-
-export default RegisterPage;
--- frontend-alibabos/src/app/(site)/update/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-function UpdatePage() {
-  return <div>Update Page</div>;
-}
-
-export default UpdatePage;
