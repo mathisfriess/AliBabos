ea6e779ded6126fa1b23d3f6774e35c6729676c8
mathis <mathis.friess@gmail.com>
2025-11-06 15:32:05 +0100

    init classes et credentials, attention credentials db a changer

---
+++ backend/BuilderService/src/main/java/com/example/builderservice/BuilderServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.builderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderServiceApplication.class, args);
+    }
+}
