0da5f2db36d747339a54dca2b95fa485b3373400
lisandre-begon <lisandre.begon1@gmail.com>
2025-12-07 01:53:49 +0100

    feat: add Spring Cloud dependencies for service discovery and update application class

--- backend/BuilderService/src/main/java/com/example/builderservice/BuilderServiceApplication.java
+++ backend/BuilderService/src/main/java/com/example/builderservice/BuilderServiceApplication.java
@@ -1,11 +1,13 @@
-package com.example.builderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BuilderServiceApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(BuilderServiceApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderServiceApplication.class, args);
+    }
 }
+
