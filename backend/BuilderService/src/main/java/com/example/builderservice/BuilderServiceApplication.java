47c3f6c1082c3d51f1650f02ac1f392f4a74b929
lisandre-begon <lisandre.begon1@gmail.com>
2025-12-07 01:55:20 +0100

    style: format main method in BuilderServiceApplication for consistency

--- backend/BuilderService/src/main/java/com/example/builderservice/BuilderServiceApplication.java
+++ backend/BuilderService/src/main/java/com/example/builderservice/BuilderServiceApplication.java
@@ -1,4 +1,3 @@
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -6,8 +5,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BuilderServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BuilderServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BuilderServiceApplication.class, args);
+  }
 }
-
