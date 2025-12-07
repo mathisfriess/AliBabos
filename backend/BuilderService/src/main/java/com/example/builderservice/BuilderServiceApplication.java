3edc31329791d5b0540bae5b4b2a523bbeb16a9f
lisandre-begon <lisandre.begon1@gmail.com>
2025-12-07 18:52:58 +0100

    style: add missing package declaration in BuilderServiceApplication

--- backend/BuilderService/src/main/java/com/example/builderservice/BuilderServiceApplication.java
+++ backend/BuilderService/src/main/java/com/example/builderservice/BuilderServiceApplication.java
@@ -1,3 +1,5 @@
+package com.example.builderservice;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
